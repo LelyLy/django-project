38acd0cb48b5a2807342459dae66ce9b624a00b8
afrolova <afrolova@bekitzur.com>
2019-07-01 16:14:07 +0300

    Add automated tests.

--- polls/views.py
+++ polls/views.py
@@ -4,6 +4,7 @@ from django.views import generic
 from django.urls import reverse
 from django.http import HttpResponseRedirect
 from .models import Choice, Question
+from django.utils import timezone
 
 
 class IndexView(generic.ListView):
@@ -11,13 +12,24 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """Return the all published questions."""
-        return Question.objects.order_by('-pub_date')
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
